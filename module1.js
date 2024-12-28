9cde837c4df798ab113496492bf8f304c7120759
deynandan <149507533+deynandan@users.noreply.github.com>
2024-12-28 11:44:00 +0000

    Add files via upload
    Nandan Dey End Point Project accompanying module1.js 28/12/24

---
+++ module1.js
@@ -0,0 +1,6 @@
+var staffnumbers = ["968009J","1111111","123456F","3456788","887722S"];
+var firstnames = ["Nandan","Cliff","Frank","Joe","Jane"];
+var lastnames = ["Data", "Davis", "Dey","Richard","Carlton"];
+var lmstaffnumbers = ["222222D","3333333","567899J","778876M","456456H"];
+var lmfirstnames = ["Peter","Hank","Geoff","Salma","Robert"];
+var lmlastnames = ["Parker","Marvin","Williams","Smith","Simmons"];
